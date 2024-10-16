946079a76c13a4a4290261169394d022c35e1e26
Gabriel Mota <44507787+gabmota8@users.noreply.github.com>
2024-10-15 23:14:01 -0300

    Add files via upload

---
+++ src/main/java/com/avaliacao/ProvaSpring/ProvaspringApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacao.ProvaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avaliacao/ProvaSpring/ProvaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.ProvaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
